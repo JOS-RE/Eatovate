340a644e46ef9a1b7a22989d1b530c00ef237284
JOS-RE <joshithreddy.g@gmail.com>
2021-06-27 15:12:32 +0530

    Finished Project

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,3 +238,34 @@
     });
 
 })()
+
+function fun1(a) {
+    if (a == 1) {
+        document.getElementById("gender").innerHTML = document.getElementById("male").innerText;
+        disp("male");
+    } else {
+        document.getElementById("gender").innerHTML = document.getElementById("female").innerText;
+        disp("female");
+    }
+}
+
+function disp(id) {
+    var T = document.getElementById(id),
+        displayValue = "";
+    if (T.style.display == "")
+        displayValue = "none";
+
+    T.style.display = displayValue;
+}
+
+function m12() {
+    disp("female");
+    document.getElementById("gender").innerHTML = document.getElementById("m2").innerHTML;
+
+}
+
+function m13() {
+    disp("female");
+    document.getElementById("gender").innerHTML = document.getElementById("m13").innerHTML;
+
+}
